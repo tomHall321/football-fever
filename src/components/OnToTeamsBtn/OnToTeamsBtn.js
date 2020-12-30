08b4d7c3f2ebec7c714fb613fd201030a40ac1de
tom <tomhall4321@gmail.com>
2020-12-30 12:15:23 +0000

    OnToTeamsBtn basic button structure added

---
+++ src/components/OnToTeamsBtn/OnToTeamsBtn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const OnToTeamsBtn = ({ tooManyPlayers }) => {
+
+    return (
+
+        <button
+            className="btn-confirms-players-teamNames"
+            onClick={}
+            disabled= {}
+        >Finished with player creation and Team Names</button>
+
+    );
+};
+
+export default OnToTeamsBtn;
