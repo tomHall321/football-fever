420df0a9b97733f4f6c295fc3af1e7bea832218d
tom <tomhall4321@gmail.com>
2020-12-31 16:25:16 +0000

    header styling, have also started styling PlayerForm and PlayerList components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
 import ResetBtn from './components/ResetBtn'
-
+import PlayerHeader from './components/Headers/PlayerHeader'
 
 function App() {
   return (
     <>
       <ResetBtn />
+      <PlayerHeader />
       <Main />
     </>
   );
--- src/components/Headers/PlayerHeader.js
+++ src/components/Headers/PlayerHeader.js
@@ -2,7 +2,6 @@ const PlayerHeader = () => {
     return (
         <header className="player-header">
             <h1>Football fever</h1>
-            <p>Select how many players, names, skill-level, and team-selection mode</p>
         </header>
     );
 };
