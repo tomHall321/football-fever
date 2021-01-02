adde944968708abc28f92a0270af517b3528782a
tom <tomhall4321@gmail.com>
2021-01-02 16:36:45 +0000

    wrappers given sensible classnames

--- src/components/RandomTeamsBtn/RandomTeamsBtn.js
+++ src/components/RandomTeamsBtn/RandomTeamsBtn.js
@@ -5,7 +5,7 @@ const RandomTeamsBtn = ({ handleCreateTeams, tooManyPlayers }) => {
     return (
 
         <button
-            className="btn-confirmsteams"
+            className="btn-confirms-teams"
             onClick={() => handleCreateTeams()}
             disabled={tooManyPlayers ? false : true}
         >Draw random teams</button>
