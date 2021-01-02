8d1e0c3f1ee5d910822cc661c1c4b61cd5b9f5dc
tom <tomhall4321@gmail.com>
2021-01-02 11:25:15 +0000

    changed skillbtn text for clarity

--- src/components/SortSkillBtn/SortSkillBtn.js
+++ src/components/SortSkillBtn/SortSkillBtn.js
@@ -8,7 +8,7 @@ const SkillTeamsBtn = ({ handleSkillTeams, tooManyPlayers }) => {
             className="btn-skill-teams"
             onClick={() => handleSkillTeams()}
             disabled={tooManyPlayers ? false : true}
-        >Draw skill teams</button>
+        >Draw fair teams</button>
 
     );
 };
